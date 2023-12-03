d3cd47475ff40178be1abd90c057a795487d3876
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-12-03 14:58:21 -0800

    Add visualization for the grid

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function createGrid(){
     const container = document.querySelector('#container');
     for(let i = 0; i < 16; i++){
-        let row = document.createElement('div');
         for(let j = 0; j < 16; j++){
             let box = document.createElement('div');
-            box.textContent = i + " " + j;
-            row.appendChild(box);
+            box.classList.add('box')
+            container.appendChild(box);
         }
-        container.appendChild(row);
     }
 }
 

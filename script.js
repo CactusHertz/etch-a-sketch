82dcf3b4cc3ad7ae202cae76f75ca0ad4fbbdc2c
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-12-03 23:36:15 -0800

    Update grid to be able to change size dynamically

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-function createGrid(){
+function createGrid(gridWidth){
     const container = document.querySelector('#container');
-    for(let i = 0; i < 16; i++){
-        for(let j = 0; j < 16; j++){
-            let box = document.createElement('div');
-            box.classList.add('box')
-            container.appendChild(box);
-        }
+    let numBoxes = gridWidth * gridWidth;
+    let ratio = 100 / gridWidth;
+    
+    for(let i = 0; i < numBoxes; i++){
+        let box = document.createElement('div');
+        box.classList.add('box')   
+        box.style.flex = '1 0 ' + ratio + '%';
+        container.appendChild(box);
     }
+
+    boxes = document.querySelectorAll('.box');
+    boxes.forEach((box1) => {
+        box1.addEventListener('mouseover', (event) => {
+            box1.style.backgroundColor = 'black';
+        })
+    });
 }
 
-createGrid();
+function deleteGrid(){
+    const container = document.querySelector('#container');
+    container.innerHTML = '';
+}
 
-boxes = document.querySelectorAll('.box');
+function promptUser(){
+    let size = prompt('Please enter grid width (max is 100):');
+    if (size > 100){
+        size = 100;
+    }
+    if (size < 1) {
+        size = 1;
+    }
 
-boxes.forEach((box1) => {
-    box1.addEventListener('mouseover', (event) => {
-        box1.style.backgroundColor = 'black';
-    })
+    deleteGrid();
+    createGrid(size);
+}
+
+createGrid(16);
+
+editButton = document.querySelector('button');
+editButton.addEventListener('click', () => {
+    promptUser();
 });
